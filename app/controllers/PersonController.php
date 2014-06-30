d087f7ce143aed51c70d2b1231c82d59e51114e4
mai7x <root@www9172uo.sakura.ne.jp>
2014-06-30 23:16:08 +0900

    POSTのデータ利用をall からonlyに変更

--- app/controllers/PersonController.php
+++ app/controllers/PersonController.php
@@ -5,10 +5,17 @@ class PersonController extends BaseController {
 
 	public function addPerson()
 	{
-		$v = Person::validate(Input::all()); //POSTデータをValidation
+		$v = Person::validate(Input::only([
+			'firstname',
+			'lastname',
+			'lastname_furigana',
+			'firstname_furigana',
+			'foreign',
+			'information'
+			])); //POSTデータをValidation
 
 		if($v->passes()) {
-			$person = Person::create(Input::all());
+			$person = Person::create(Input::only(['company_id','department','position','postcode','address','email','tel','fax']));
 			$card = new Card(Input::all());
 			$card = $person->card()->save($card);
 //			Card::create(Input::all());
